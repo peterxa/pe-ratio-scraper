d88884d7db90378062c3099cae9897f6f7dba61b
peterxa <93025156+peterxa@users.noreply.github.com>
2025-04-23 00:06:39 -0500

    Subiendo API FastAPI-dividendo a Render

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ def get_pe_ratio(ticker: str):
     
     except Exception as e:
         return {"error": str(e)}
+
+@app.get("/dividend/{ticker}")
+def get_dividend(ticker: str):
+    try:
+        stock = yf.Ticker(ticker)
+        dividend = stock.info.get("dividendRate")
+        dividend_yield = stock.info.get("dividendYield")
+
+        if dividend is None:
+            return {"ticker": ticker, "error": "Dividendo no disponible"}
+
+        return {
+            "ticker": ticker,
+            "dividend": dividend,
+            "dividend_yield": f"{dividend_yield * 100:.2f}%" if dividend_yield else None
+        }
+    
+    except Exception as e:
+        return {"error": str(e)}
